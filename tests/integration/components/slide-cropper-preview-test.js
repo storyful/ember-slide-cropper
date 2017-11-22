5d37300a058b6c77cfd44733001c0c4643d5d992
Bruno Tarmann <bruno@tarmann.com.br>
2017-11-22 10:59:17 +0000

    Fix old name on test

--- tests/integration/components/slide-cropper-preview-test.js
+++ tests/integration/components/slide-cropper-preview-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('slide-crop-preview', 'Integration | Component | slide crop preview', {
+moduleForComponent('slide-cropper-preview', 'Integration | Component | slide crop preview', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{slide-crop-preview}}`);
+  this.render(hbs`{{slide-cropper-preview}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#slide-crop-preview}}
+    {{#slide-cropper-preview}}
       template block text
-    {{/slide-crop-preview}}
+    {{/slide-cropper-preview}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
